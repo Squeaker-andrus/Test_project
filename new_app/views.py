229b6ae6d37b589384cc702b36216b53acbdcff5
Squeaker-andrus <andrussvarog1024@gmail.com>
2023-02-13 09:14:32 +0300

    Test

--- new_app/views.py
+++ new_app/views.py
@@ -3,3 +3,4 @@ from .models import Treads
 
 def show_threads(request):
     all_threads = Treads.objects.all()
+    return render(request, "main_page.html", {"posts": all_threads})
--- newproject/urls.py
+++ newproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from new_app import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.show_threads)
 ]
